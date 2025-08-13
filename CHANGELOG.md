1c68edb16ab36bfd35257065f9166491cfdd6159
bougueil <rmariana@gmail.com>
2025-08-13 14:40:53 +0200

    Release v0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog for pcache
+
+## v0.1.1 (2025-08-13)
+
+ * Add content/0
+
+## v0.1.0 (2025-08-04)
+
+  * First release.
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pcache.MixProject do
   def project do
     [
       app: :pcache,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       package: package(),
       start_permanent: Mix.env() == :prod,
